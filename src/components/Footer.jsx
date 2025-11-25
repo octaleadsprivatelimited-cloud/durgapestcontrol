7da2cb73e0a633997c925a3fc31983c130ef34fd
Octaleads <octaleadsprivatelimited@gmail.com>
2025-11-25 13:03:07 +0530

    Swap footer social link to Facebook

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -190,14 +190,14 @@ export default function Footer() {
 										</svg>
 									</a>
 									<a
-										href="https://www.youtube.com/@Durgabhavanipestcontrolservice"
+										href="https://www.facebook.com/share/1BowNeFzee/"
 										className="social-link"
-										aria-label="YouTube"
+										aria-label="Facebook"
 										target="_blank"
 										rel="noopener noreferrer"
 									>
 										<svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
-											<path d="M23.498 6.186a2.93 2.93 0 00-2.06-2.071C19.557 3.64 12 3.64 12 3.64s-7.557 0-9.438.475a2.93 2.93 0 00-2.06 2.07A30.537 30.537 0 000 11.996a30.54 30.54 0 00.502 5.81 2.93 2.93 0 002.06 2.071c1.881.475 9.438.475 9.438.475s7.557 0 9.438-.475a2.93 2.93 0 002.06-2.07 30.543 30.543 0 00.502-5.81 30.538 30.538 0 00-.502-5.81zM9.75 15.02V8.972l6.26 3.024-6.26 3.024z"/>
+											<path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
 										</svg>
 									</a>
 									<a
